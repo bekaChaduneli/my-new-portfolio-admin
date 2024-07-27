070ca9679119b9a31f97b9a4f07c8f8e489bd9ae
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2024-07-27 18:06:49 +0400

    add books query

--- src/graphql/query.ts
+++ src/graphql/query.ts
@@ -1,12 +1,80 @@
 import { gql } from "@apollo/client";
 
 export const GET_BOOKS = gql`
-  query GetBooks {
-    books {
+  query findManyBooks {
+    findManyBooks {
+      finished
       id
-      title
-      author
+      image
+      index
+      link
+      pages
+      readedPages
       releaseDate
+      type
+      translations(where: {}) {
+        updatedAt
+        title
+        description
+        createdAt
+        booksId
+        author
+        id
+        languageCode
+      }
     }
   }
 `;
+
+export const GET_BOOK = gql`
+  query findUniqueBooks($id: BookInput!) {
+    findUniqueBooks(where: { id: $id }) {
+      finished
+      id
+      image
+      index
+      link
+      pages
+      readedPages
+      releaseDate
+      type
+      translations {
+        updatedAt
+        title
+        languageCode
+        id
+        description
+        createdAt
+        booksId
+        author
+      }
+    }
+  }
+`;
+
+export const GET_BOOK_BY_TYPE = gql`
+  query findManyBooks($type: BookType!) {
+    findManyBooks(where: {type: {equals: $type}}) {
+    finished
+    id
+    image
+    index
+    link
+    pages
+    readedPages
+    releaseDate
+    type
+    translations {
+      updatedAt
+      title
+      description
+      createdAt
+      booksId
+      author
+      id
+      languageCode
+    }
+  }
+  }
+`
+
