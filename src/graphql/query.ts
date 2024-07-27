cb6a0c720ec470ac8f86d2916ef2a50939dfeeca
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2024-07-27 18:12:58 +0400

    add about me query

--- src/graphql/query.ts
+++ src/graphql/query.ts
@@ -53,7 +53,7 @@ export const GET_BOOK = gql`
 `;
 
 export const GET_BOOK_BY_TYPE = gql`
-  query findManyBooks($type: BookType!) {
+  query findManyBooks($type: BookInput!) {
     findManyBooks(where: {type: {equals: $type}}) {
     finished
     id
@@ -73,6 +73,130 @@ export const GET_BOOK_BY_TYPE = gql`
       author
       id
       languageCode
+    }
+  }
+  }
+`
+
+export const GET_ABOUTME = gql`
+  query aboutMe($id: AboutMeInput) {
+    aboutMe(where: {id: $id}) {
+    works {
+      updatedAt
+      translations {
+        worksId
+        updatedAt
+        role
+        locationType
+        location
+        languageCode
+        id
+        employmentType
+        description
+        createdAt
+        company
+      }
+      toDate
+      link
+      id
+      fromDate
+      createdAt
+      aboutMeId
+      aboutMe {
+        updatedAt
+        translations {
+          updatedAt
+          role
+          name
+          languageCode
+          createdAt
+          id
+          country
+          city
+          aboutMeId
+          about
+        }
+        age
+        createdAt
+        experience
+        id
+        image
+        projectNum
+      }
+    }
+    age
+    certificates {
+      aboutMeId
+      createdAt
+      expirationDate
+      image
+      id
+      issueDate
+      link
+      translations {
+        certificatesId
+        createdAt
+        description
+        id
+        languageCode
+        name
+        organiation
+        updatedAt
+      }
+      updatedAt
+    }
+    createdAt
+    education {
+      createdAt
+      fromDate
+      id
+      link
+      toDate
+      updatedAt
+      translations {
+        createdAt
+        degree
+        description
+        educationsId
+        fieldOfStudy
+        gpa
+        id
+        languageCode
+        name
+        updatedAt
+      }
+    }
+    experience
+    id
+    image
+    projectNum
+    updatedAt
+    languages {
+      id
+      createdAt
+      aboutMeId
+      updatedAt
+      translations {
+        updatedAt
+        name
+        level
+        languagesId
+        languageCode
+        id
+        description
+        createdAt
+      }
+    }
+    translations {
+      updatedAt
+      role
+      name
+      languageCode
+      id
+      createdAt
+      country
+      city
+      about
     }
   }
   }
