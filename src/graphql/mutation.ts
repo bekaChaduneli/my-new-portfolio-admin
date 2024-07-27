91e6e129b335b905245fe933462f0cc6de703d50
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2024-07-27 15:49:35 +0400

    add book blog and login mutation

--- src/graphql/mutation.ts
+++ src/graphql/mutation.ts
@@ -9,3 +9,50 @@ export const CREATE_BOOK = gql`
     }
   }
 `;
+
+export const DELETE_BOOK = gql`
+  mutation DeleteOneBooks($id: BookInput!) {
+    deleteOneBooks(where: { id: $id }) {
+      id
+    }
+  }
+`;
+
+export const LOGIN = gql`
+  mutation loginAdmin($email: String!, $password: String!) {
+    loginAdmin(email: $email, password: $password) {
+      token
+      admin {
+        id
+      }
+    }
+  }
+`;
+
+export const VALIDATE_TOKEN = gql`
+  mutation validateToken($token: String!) {
+    validateToken(token: $token) {
+      isValid
+      token
+      admin {
+        id
+      }
+    }
+  }
+`;
+
+export const CREATE_BLOG = gql`
+  mutation CreateBLOG($input: BlogInput!) {
+    createBlogs(data: $input) {
+      id
+    }
+  }
+`;
+
+export const DELETE_BLOG = gql`
+  mutation DeleteOneBlogs($id: BookInput!) {
+    deleteOneBlogs(where: { id: $id }) {
+      id
+    }
+  }
+`;
