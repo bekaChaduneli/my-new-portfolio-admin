e9eea7cd619b1f9407cde14e19e37ecdc6017f0e
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2024-07-31 16:48:01 +0400

    add services page

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,3 +1,285 @@
-export default function Services() {
-  return <div>Services</div>;
-}
+import { useEffect, useState } from "react";
+import { useMutation, useQuery } from "@apollo/client";
+import { Button, List, Form, message, Modal, Input } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+import { IServices } from "../types/Services";
+import { GET_SERVICES } from "@graphql/query";
+import { uploadToCloudinary } from "../services/cloudinaryService";
+import Dragger from "antd/es/upload/Dragger";
+import {
+  CREATE_SERVICES,
+  DELETE_SERVICES,
+  UPDATE_SERVICES,
+} from "@graphql/mutation";
+
+const Services = () => {
+  const [deleteOneService] = useMutation(DELETE_SERVICES, {
+    refetchQueries: [{ query: GET_SERVICES }],
+    onCompleted: () => {
+      message.success("Services deleted successfully!");
+    },
+    onError: (error) => {
+      console.log(error.message);
+      message.error(error.message);
+    },
+  });
+  const handleDelete = (id: string) => {
+    deleteOneService({ variables: { id } });
+  };
+
+  const [form] = Form.useForm();
+  const [currentServices, setCurrentServices] = useState<IServices | null>(
+    null
+  );
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
+  const [loadingBackground, setLoadingBackground] = useState<boolean>(false);
+  const [background, setBackground] = useState<string | null | undefined>(null);
+
+  const handleFileUpload = async (file: File) => {
+    try {
+      setLoadingBackground(true);
+      const result = await uploadToCloudinary(file);
+      setBackground(result.secure_url);
+    } catch {
+      message.error("Error uploading file.");
+    } finally {
+      setLoadingBackground(false);
+    }
+  };
+
+  const { data, loading, error } = useQuery(GET_SERVICES);
+  useEffect(() => {
+    if (currentServices) {
+      currentServices && setBackground(currentServices.background);
+    } else {
+      setBackground(null);
+    }
+  }, [currentServices, isModalVisible]);
+  const [createOneServices] = useMutation(CREATE_SERVICES, {
+    refetchQueries: [{ query: GET_SERVICES }],
+    onCompleted: () => message.success("Service created successfully!"),
+    onError: (error) => message.error(error.message),
+  });
+
+  const [updateOneServices] = useMutation(UPDATE_SERVICES, {
+    refetchQueries: [{ query: GET_SERVICES }],
+    onCompleted: () => message.success("Service updated successfully!"),
+    onError: (error) => message.error(error.message),
+  });
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  const handleCreate = async (values: any) => {
+    try {
+      await createOneServices({
+        variables: {
+          input: {
+            background: background,
+            order: values.order,
+            translations: {
+              createMany: {
+                data: [
+                  {
+                    name: values.enName,
+                    description: values.enDescription,
+                    languageCode: "en",
+                  },
+                  {
+                    name: values.kaName,
+                    description: values.kaDescription,
+                    languageCode: "ka",
+                  },
+                ],
+              },
+            },
+          },
+        },
+      });
+      form.resetFields();
+      setIsModalVisible(false);
+    } catch (error) {
+      console.error("Error creating service:", error);
+    }
+  };
+
+  const handleUpdate = async (values: any) => {
+    try {
+      await updateOneServices({
+        variables: {
+          id: currentServices?.id,
+          data: {
+            background: { set: background },
+            order: { set: values.order },
+            translations: {
+              updateMany: [
+                {
+                  where: { languageCode: { equals: "en" } },
+                  data: {
+                    name: { set: values.enName },
+                    description: { set: values.enDescription },
+                  },
+                },
+                {
+                  where: { languageCode: { equals: "ka" } },
+                  data: {
+                    name: { set: values.kaName },
+                    description: { set: values.kaDescription },
+                  },
+                },
+              ],
+            },
+          },
+        },
+      });
+      form.resetFields();
+      setIsModalVisible(false);
+    } catch (error) {
+      console.error("Error updating service:", error);
+    }
+  };
+
+  const handleCancel = () => {
+    setCurrentServices(null);
+    setBackground(null);
+    setIsModalVisible(false);
+    form.resetFields();
+  };
+
+  const handleEdit = (services: IServices) => {
+    setCurrentServices(services);
+    setIsModalVisible(true);
+
+    const en = services.translations.find((t) => t.languageCode === "en");
+    const ka = services.translations.find((t) => t.languageCode === "ka");
+
+    const initialValues = {
+      order: services?.order,
+      enDescription: en?.description,
+      enName: en?.name,
+      kaDescription: ka?.description,
+      kaName: ka?.name,
+    };
+    form.setFieldsValue(initialValues);
+  };
+
+  return (
+    <div>
+      <Button
+        type="primary"
+        icon={<PlusOutlined />}
+        onClick={() => setIsModalVisible(true)}
+      >
+        Add New Service
+      </Button>
+
+      <List
+        dataSource={data?.findManyServices || []}
+        renderItem={(services: IServices) => (
+          <List.Item
+            actions={[
+              <Button type="link" onClick={() => handleEdit(services)}>
+                Edit
+              </Button>,
+              <Button
+                type="link"
+                danger
+                onClick={() => handleDelete(services.id)}
+              >
+                Delete
+              </Button>,
+            ]}
+          >
+            <List.Item.Meta
+              title={
+                services.translations.find((t: any) => t.languageCode === "en")
+                  ?.name
+              }
+            />
+          </List.Item>
+        )}
+      />
+
+      <Modal
+        title={currentServices ? "Edit Service" : "Create Service"}
+        visible={isModalVisible}
+        onCancel={handleCancel}
+        width={1200}
+        onOk={() => {
+          form.validateFields().then((values) => {
+            if (currentServices) {
+              handleUpdate(values);
+            } else {
+              handleCreate(values);
+            }
+          });
+        }}
+      >
+        <Form
+          form={form}
+          layout="vertical"
+          onFinish={currentServices ? handleUpdate : handleCreate}
+        >
+          <Form.Item label="Background">
+            <Dragger
+              name="file"
+              customRequest={({ file, onSuccess }) => {
+                handleFileUpload(file as File);
+                onSuccess?.({});
+              }}
+              showUploadList={false}
+            >
+              <p className="ant-upload-drag-icon">
+                <PlusOutlined />
+              </p>
+              <p className="ant-upload-text">
+                Click or drag to upload background
+              </p>
+            </Dragger>
+            {background && (
+              <img
+                src={background}
+                alt="uploaded"
+                style={{ width: 100, height: 100, marginTop: 10 }}
+              />
+            )}
+          </Form.Item>
+          <Form.Item label="Order" name="order" rules={[{ required: true }]}>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="English Name"
+            name="enName"
+            rules={[{ required: true }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Georgian Name"
+            name="kaName"
+            rules={[{ required: true }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="English Description"
+            name="enDescription"
+            rules={[{ required: true }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Georgian Description"
+            name="kaDescription"
+            rules={[{ required: true }]}
+          >
+            <Input />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </div>
+  );
+};
+
+export default Services;
