3a29efcda35262b30bd72a4fbf699e696659e0d6
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2024-08-01 21:11:17 +0400

    fix blogs page

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useMutation, useQuery } from "@apollo/client";
 import { CREATE_BLOG, DELETE_BLOGS, UPDATE_BLOGS } from "@graphql/mutation";
-import { Button, List, Form, message, Modal, Input } from "antd";
+import { Button, List, Form, message, Modal, Input, Row, Col } from "antd";
 import { IBlog, IBlogsResponse, IBlogTranslation } from "../types/Blogs";
-import { FileUpload } from "@components/FileUpload";
+import { PlusOutlined } from "@ant-design/icons";
 import { uploadToCloudinary } from "../services/cloudinaryService";
 import { GET_BLOGS } from "@graphql/query";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import styled from "styled-components";
+import Dragger from "antd/es/upload/Dragger";
 
 const StyledReactQuill = styled(ReactQuill)`
   height: 300px;
@@ -34,7 +35,28 @@ const Blogs = () => {
 
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
   const [loadingImage, setLoadingImage] = useState<boolean>(false);
-  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const [image, setImage] = useState<string | null>(null);
+
+  const handleFileUpload = async (file: File) => {
+    try {
+      setLoadingImage(true);
+      const result = await uploadToCloudinary(file);
+      const url = result.secure_url;
+      setImage(url);
+    } catch (error) {
+      message.error("Error uploading file.");
+    } finally {
+      setLoadingImage(false);
+    }
+  };
+
+  useEffect(() => {
+    if (currentBlog) {
+      currentBlog && setImage(currentBlog.background);
+    } else {
+      setImage(null);
+    }
+  }, [currentBlog]);
 
   const [createOneBlogs] = useMutation(CREATE_BLOG, {
     refetchQueries: [{ query: GET_BLOGS }],
@@ -65,25 +87,12 @@ const Blogs = () => {
     return <div>Error: {error.message}</div>;
   }
 
-  const handleImageUpload = async (file: File) => {
-    try {
-      setLoadingImage(true);
-      const result = await uploadToCloudinary(file);
-      setImageUrl(result.secure_url);
-      form.setFieldsValue({ background: result.secure_url });
-    } catch (error) {
-      message.error("Error uploading image.");
-    } finally {
-      setLoadingImage(false);
-    }
-  };
-
   const handleCreate = (values: any) => {
     createOneBlogs({
       variables: {
         data: {
           link: values.link,
-          background: values.background,
+          background: image,
           translations: {
             createMany: {
               data: [
@@ -105,6 +114,9 @@ const Blogs = () => {
         },
       },
     });
+    form.resetFields();
+    setCurrentBlog(null);
+    setIsModalVisible(false);
   };
 
   const handleUpdate = (values: any) => {
@@ -113,7 +125,7 @@ const Blogs = () => {
         id: currentBlog?.id,
         data: {
           link: { set: values.link },
-          background: { set: values.background },
+          background: { set: image },
           translations: {
             updateMany: [
               {
@@ -137,13 +149,16 @@ const Blogs = () => {
         },
       },
     });
+    form.resetFields();
+    setCurrentBlog(null);
+    setIsModalVisible(false);
   };
 
   const handleCancel = () => {
     setCurrentBlog(null);
     form.resetFields();
     setValue("");
-    setImageUrl(null);
+    setImage(null);
     setIsModalVisible(false);
   };
 
@@ -160,7 +175,7 @@ const Blogs = () => {
 
     const initialValues = {
       link: blog.link,
-      background: blog.background,
+      background: image,
       enHeadline: en?.headline,
       enAbout: en?.about,
       enMarkdown: en?.markdown,
@@ -170,7 +185,6 @@ const Blogs = () => {
     };
 
     form.setFieldsValue(initialValues);
-    setValue(en?.about || "");
   };
 
   const handleDelete = (id: string) => {
@@ -179,83 +193,103 @@ const Blogs = () => {
 
   return (
     <div>
-      <Button
-        type="primary"
-        onClick={() => {
-          setCurrentBlog(null);
-          form.resetFields();
-          setValue("");
-          setImageUrl(null);
-          setIsModalVisible(true);
-        }}
-      >
-        Add Blog
+      <Button type="primary" onClick={() => setIsModalVisible(true)}>
+        Add New Blog
       </Button>
       <List
         dataSource={data?.findManyBlogs}
-        renderItem={(item: IBlog) => (
+        renderItem={(blog: IBlog) => (
           <List.Item
             actions={[
-              <Button type="link" onClick={() => handleEdit(item)}>
+              <Button type="link" onClick={() => handleEdit(blog)}>
                 Edit
               </Button>,
-              <Button type="link" danger onClick={() => handleDelete(item.id)}>
+              <Button type="link" danger onClick={() => handleDelete(blog.id)}>
                 Delete
               </Button>,
             ]}
           >
-            {item.translations.find((t) => t.languageCode === "en")?.headline}
+            <List.Item.Meta
+              title={
+                blog.translations.find((t) => t.languageCode === "en")?.headline
+              }
+            />
           </List.Item>
         )}
       />
       <Modal
         visible={isModalVisible}
         onCancel={handleCancel}
-        footer={null}
         width={1200}
-      >
-        <Form
-          form={form}
-          layout="vertical"
-          onFinish={(values) => {
+        onOk={() => {
+          form.validateFields().then((values) => {
             if (currentBlog) {
               handleUpdate(values);
             } else {
               handleCreate(values);
             }
-            handleCancel();
-          }}
+            setIsModalVisible(false);
+          });
+        }}
+      >
+        <Form
+          form={form}
+          layout="vertical"
+          onFinish={currentBlog ? handleUpdate : handleCreate}
         >
-          <Form.Item label="Background Image" name="background">
-            <FileUpload onUpload={handleImageUpload} />
-            {loadingImage && <p>Loading...</p>}
+          <Form.Item label="Image">
+            <Dragger
+              name="file"
+              customRequest={({ file, onSuccess }) => {
+                handleFileUpload(file as File);
+                onSuccess?.({});
+              }}
+              showUploadList={false}
+            >
+              <p className="ant-upload-drag-icon">
+                <PlusOutlined />
+              </p>
+              <p className="ant-upload-text">
+                Click or drag to upload background
+              </p>
+            </Dragger>
+            {image && (
+              <div style={{ marginTop: 10 }}>
+                <img
+                  src={image}
+                  alt={`background`}
+                  style={{ width: 100, height: 100, marginRight: 10 }}
+                />
+              </div>
+            )}
           </Form.Item>
           <Form.Item label="Link" name="link">
             <Input />
           </Form.Item>
-          <Form.Item label="English Headline" name="enHeadline">
-            <Input />
-          </Form.Item>
-          <Form.Item label="English About" name="enAbout">
-            <Input.TextArea />
-          </Form.Item>
-          <Form.Item label="English Markdown" name="enMarkdown">
-            <StyledReactQuill theme="snow" />
-          </Form.Item>
-          <Form.Item label="Georgian Headline" name="kaHeadline">
-            <Input />
-          </Form.Item>
-          <Form.Item label="Georgian About" name="kaAbout">
-            <Input.TextArea />
-          </Form.Item>
-          <Form.Item label="Georgian Markdown" name="kaMarkdown">
-            <StyledReactQuill theme="snow" />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Form.Item label="English Headline" name="enHeadline">
+                <Input />
+              </Form.Item>
+              <Form.Item label="English About" name="enAbout">
+                <Input.TextArea />
+              </Form.Item>
+              <Form.Item label="English Markdown" name="enMarkdown">
+                <StyledReactQuill theme="snow" />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item label="Georgian Headline" name="kaHeadline">
+                <Input />
+              </Form.Item>
+              <Form.Item label="Georgian About" name="kaAbout">
+                <Input.TextArea />
+              </Form.Item>
+              <Form.Item label="Georgian Markdown" name="kaMarkdown">
+                <StyledReactQuill theme="snow" />
+              </Form.Item>
+            </Col>
+          </Row>
         </Form>
       </Modal>
     </div>
