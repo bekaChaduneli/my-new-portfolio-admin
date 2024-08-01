cfd09c3cc8b5fb610f751cfc34914d051ebe7e2c
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2024-08-01 21:27:02 +0400

    fix profile

--- src/types/Profile.ts
+++ src/types/Profile.ts
@@ -28,3 +28,26 @@ export interface IProfile {
 export interface IProfileResponse {
   findFirstProfile: IProfile;
 }
+
+export interface ProfileInitialValues {
+  image: string | null;
+  resume: string | null;
+  age: string | undefined;
+  mail: string | undefined;
+  enName: string | undefined;
+  kaName: string | undefined;
+  enSurname: string | undefined;
+  kaSurname: string | undefined;
+  enProfession: string | undefined;
+  kaProfession: string | undefined;
+  enLocation: string | undefined;
+  kaLocation: string | undefined;
+  enExperience: string | undefined;
+  kaExperience: string | undefined;
+  enUniversity: string | undefined;
+  kaUniversity: string | undefined;
+  enUniversityAbout: string | undefined;
+  kaUniversityAbout: string | undefined;
+  enAboutMe: string | undefined;
+  kaAboutMe: string | undefined;
+}
