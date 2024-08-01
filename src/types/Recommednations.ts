e3ec189a1bf26caa16d004c06df8dbb060de503f
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2024-08-01 21:38:15 +0400

    fix recommendations

--- src/types/Recommednations.ts
+++ src/types/Recommednations.ts
@@ -22,3 +22,15 @@ export interface IRecommendations {
 export interface IRecommendationsResponse {
   findManyRecommendations: IRecommendations;
 }
+
+export interface RecommendationsInitialValues {
+  date: string | undefined;
+  enDescription: string | undefined;
+  enName: string | undefined;
+  enBio: string | undefined;
+  enRole: string | undefined;
+  kaDescription: string | undefined;
+  kaName: string | undefined;
+  kaBio: string | undefined;
+  kaRole: string | undefined;
+}
